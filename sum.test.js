fe5b728cab4aa6b6f46df7bf5f598b5584207d47
fb0801 <fbhatti0801@gmail.com>
2024-02-23 15:22:10 +0000

    test 9 async testing

--- sum.test.js
+++ sum.test.js
@@ -3,3 +3,8 @@ const fetchPromise = require("./sum")
 test('the data is peanut butter', () => {
     return expect(fetchPromise()).resolves.toBe('peanut butter')
 })
+
+
+test('the fetch fails with an error', () => {
+    return expect(fetchPromise()).rejects.toThrow('error')
+})
